8cfbba4b3a6e0fba0f38794d4042efe327b9646f
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-18 17:59:33 +0200

    remove objects array

--- main.js
+++ main.js
@@ -3,52 +3,6 @@ Utilizzando Postman, testiamo una chiamata all’endpoint di JSON Placeholder:
 https://jsonplaceholder.typicode.com/photos?_limit=6
 Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come poterli sfruttare.*/
 
-//copio l'array di oggetti
-const albums= [
-    {
-        "albumId": 1,
-        "id": 1,
-        "title": "accusamus beatae ad facilis cum similique qui sunt",
-        "url": "https://via.placeholder.com/600/92c952",
-        "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-    },
-    {
-        "albumId": 1,
-        "id": 2,
-        "title": "reprehenderit est deserunt velit ipsam",
-        "url": "https://via.placeholder.com/600/771796",
-        "thumbnailUrl": "https://via.placeholder.com/150/771796"
-    },
-    {
-        "albumId": 1,
-        "id": 3,
-        "title": "officia porro iure quia iusto qui ipsa ut modi",
-        "url": "https://via.placeholder.com/600/24f355",
-        "thumbnailUrl": "https://via.placeholder.com/150/24f355"
-    },
-    {
-        "albumId": 1,
-        "id": 4,
-        "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-        "url": "https://via.placeholder.com/600/d32776",
-        "thumbnailUrl": "https://via.placeholder.com/150/d32776"
-    },
-    {
-        "albumId": 1,
-        "id": 5,
-        "title": "natus nisi omnis corporis facere molestiae rerum in",
-        "url": "https://via.placeholder.com/600/f66b97",
-        "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
-    },
-    {
-        "albumId": 1,
-        "id": 6,
-        "title": "accusamus ea aliquid et amet sequi nemo",
-        "url": "https://via.placeholder.com/600/56a8c2",
-        "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
-    }
-]
-
 //richiamo l'elemento row
 const albumsRow = document.querySelector('.row');
 
