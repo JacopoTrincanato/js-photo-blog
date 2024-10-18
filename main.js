b81d012c2c915de125f076740a2ef2ec34bea4e4
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-18 11:38:28 +0200

    create main.js file

---
+++ main.js
@@ -0,0 +1,4 @@
+/*Milestone 2
+Utilizzando Postman, testiamo una chiamata all’endpoint di JSON Placeholder:
+https://jsonplaceholder.typicode.com/photos?_limit=6
+Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come poterli sfruttare.*/
