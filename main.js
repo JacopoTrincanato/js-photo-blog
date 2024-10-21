c02dd0ddde7e88f85dde120ff8e702b677f2e603
JacopoTrincanato <trincanatoj@gmail.com>
2024-10-21 12:59:06 +0200

    create a for cicle for give eventListener to images

--- main.js
+++ main.js
@@ -12,72 +12,88 @@ const overlay = document.querySelector('.overlay');
 //effettuo una chiamata AJAX
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
 .then(response =>{
-console.log(response);
+    console.log(response);
 
-const albumsRowEl = response.data;
-console.log(albumsRowEl);
+    const albumsRowEl = response.data;
+    console.log(albumsRowEl);
 
-//ciclo all'interno di albumsEl
+    //ciclo all'interno di albumsEl
 
-albumsRowEl.forEach(album => {
+    albumsRowEl.forEach(album => {
 
-    //trasformo la prima lettera di ogni parola in una maiuscola
-    let capitalizedString = (str)=> str[0].toUpperCase() + str.slice(1).toLowerCase();
-    //creo una variabile words dove splitto le stringhe dei vari titoli
-    let words = album.title.split(' ').map(capitalizedString)
-    //ritrasformo gli array in stringhe e li salvo in una variabile
-    let capitalizedTitle = words.join(' ') 
-    album.title = capitalizedTitle
-    console.log(album.title);
-    
-    //destrutturo
-    const {title, url} = album;
+        //trasformo la prima lettera di ogni parola in una maiuscola
+        let capitalizedString = (str)=> str[0].toUpperCase() + str.slice(1).toLowerCase();
+        //creo una variabile words dove splitto le stringhe dei vari titoli
+        let words = album.title.split(' ').map(capitalizedString)
+        //ritrasformo gli array in stringhe e li salvo in una variabile
+        let capitalizedTitle = words.join(' ') 
+        album.title = capitalizedTitle
+        console.log(album.title);
+        
+        //destrutturo
+        const {title, url} = album;
 
-    //creo il markup da inserire dentro row
-    const markup = `
-    <div class="col-lg-4 col-md-6 col-sm-12">
-        <div class="album album-md album-sm">
-            <div class="circle">
-                <img src="./img/pin.svg" alt="">
+        //creo il markup da inserire dentro row
+        const markup = `
+        <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="album album-md album-sm">
+                <div class="circle">
+                    <img src="./img/pin.svg" alt="">
+                    </div>
+                    <div class="album-img">
+                        <img src="${url}" alt="">
+                    </div>
+                <div class="album-description">
+                    <p>${title}</p>
                 </div>
-                <div class="album-img">
-                    <img src="${url}" alt="">
-                </div>
-            <div class="album-description">
-                <p>${title}</p>
             </div>
         </div>
-    </div>
-    `
-    
-    //inserisco il markup nell'HTML
-    albumsRow.innerHTML += markup
+        `
+        
+        //inserisco il markup nell'HTML
+        albumsRow.innerHTML += markup 
 
-    
-    
-});
-
-let image = albumsRow.querySelector('.album-img img');
-
-//aggiungo un evento che faccia ricomparire l'overlay
-image.addEventListener('click', ()=> {
-    overlay.classList.remove('disappear');
-    //inserisco il markup dell'overlay nell'HTML
-    overlay.innerHTML = `
-        <img src="${image}" alt="">
+        //inserisco il markup dell'overlay nell'HTML
+        overlay.innerHTML = `
+        <img src="${url}" alt="">
         <button>Close</button>
-    `
+        `
+        
+        
 
-});
+        let image = albumsRow.querySelectorAll('.album-img img');
+
+        for (let i = 0; i < image.length; i++) {
+            const element = image[i];
+            
+            
+            //aggiungo un evento che faccia ricomparire l'overlay
+            element.addEventListener('click', ()=> {
+                overlay.classList.remove('disappear');
+                    
+                //richiamo il bottone
+                const button = document.querySelector('button');
+
+                //aggiungo un evento al bottone che riaggiunga la classe disappear all'overlay
+                button.addEventListener('click', ()=>{
+                    overlay.classList.add('disappear');
+                });
+                });
+            
 
 
-//richiamo il bottone
-const button = document.querySelector('button');
+        }
 
-//aggiungo un evento al bottone che riaggiunga la classe disappear all'overlay
-button.addEventListener('click', ()=>{
-    overlay.classList.add('disappear');
-});
+    });
+
+
+
+    
+
+    
+
+
+    
 
 
 //aggiungo un messaggio di errore
